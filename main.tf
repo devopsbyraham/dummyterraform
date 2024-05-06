8085c5e3ff8965767bed10eadd7217aecbf6c0f4
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2024-05-06 17:33:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ region = "us-east-1"
 resource "aws_instance" "three" {
   ami           = "ami-00b8917ae86a424c9"
   instance_type = "t2.medium"
-  key_name      = "dummykey"
+  key_name      = "apr6pm"
   tags = {
     Name = "n.virginia-server"
   }
