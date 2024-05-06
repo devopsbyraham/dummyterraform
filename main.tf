8c77d675f2939e319aa742b5ad3240ba88d7263a
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2024-05-06 17:25:39 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_instance" "three" {
+  ami           = "ami-00b8917ae86a424c9"
+  instance_type = "t2.medium"
+  key_name      = "dummykey"
+  tags = {
+    Name = "n.virginia-server"
+  }
+}
