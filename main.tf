e96de6503e88b2fa835287eea49fa1edc6a9e87a
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2024-11-22 21:49:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,22 @@ provider "aws" {
 region = "us-east-1"
 }
 
+terraform {
+  backend "remote" {
+    organization = "raham"
+
+    workspaces {
+      name = "dummyterraform"
+    }
+  }
+}
+
+
 resource "aws_instance" "three" {
   count         =  3
   ami           = "ami-00b8917ae86a424c9"
   instance_type = "t2.medium"
-  key_name      = "mykeypair9am"
+
   tags = {
     Name = "mumbai-server"
   }
